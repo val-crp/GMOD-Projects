92457e58298bf486cd6fc3f421f054489c2b1614
val-vicky <93606438+val-vicky@users.noreply.github.com>
2025-01-10 15:39:29 +0100

    Originally designed for French Users in mind, which is the reason for some of the French text.

---
+++ Missive System/items/base/sh_missiveprv.lua
@@ -0,0 +1,10 @@
+ITEM.name = "Base Missive"
+ITEM.description = ""
+ITEM.category = "Other"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.isBase = true  
+
+function ITEM:GetDescription()
+    return self.description
+end
--- systeme_missive/sh_plugin.lua
+++
@@ -1,5 +0,0 @@
-PLUGIN.name = "Système Missive"
-PLUGIN.description = ""
-PLUGIN.author = "Val"
-
-ix.util.Include( "cl_plugin.lua" )
